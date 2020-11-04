2d5dbd43c522958ffd7b14b5024bbdbf2b780a67
Le Minh Quan <quanle96@outlook.com>
2020-11-04 18:00:38 +0700

    WIP add redux-saga

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./component/Header";
-import Body from "./component/Body";
-import Footer from "./component/Footer";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import createSagaMiddleware from 'redux-saga';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+import reducer from './redux/reducers';
+import rootSaga from './redux/sagas';
+
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './component/Header';
+import Body from './component/Body';
+import Footer from './component/Footer';
+
+const sageMidleWare = createSagaMiddleware();
+const store = createStore(reducer, applyMiddleware(sageMidleWare));
+
+sageMidleWare.run(rootSaga);
+
 class Home extends React.Component {
   render() {
     return (
-      <div>
+      <Provider store={store}>
         <Header />
         <Body />
         <Footer />
-      </div>
+      </Provider>
     );
   }
 }
 
-ReactDOM.render(<Home />, document.getElementById("root"));
+ReactDOM.render(<Home />, document.getElementById('root'));
